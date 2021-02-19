7e9708de20e4a5a1ca1060aaf54a744fdf854bf2
epe52 <36105192+epe52@users.noreply.github.com>
2021-02-19 19:21:13 +0200

    fix: added prettier confiq and spotify search functionality

---
+++ src/component/SearchFromSpotify.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const SearchResults = ({ results, searchType }) => {
+  const type = `${searchType.toLowerCase()}s`;
+  const items = results[type]?.items;
+  // console.log('Results GEGE', results[type]?.items);
+  // TO-DO: show more info from results
+  return (
+    <>
+      {items?.map((item) => (
+        <li key={item?.name}>{item?.name}</li>
+      ))}
+    </>
+  );
+};
+
+const SearchFromSpotify = ({ accessToken }) => {
+  const [search, setSearch] = useState([]);
+  const [searchType, setSearchType] = useState('artist');
+  const [searchResult, setSearchResult] = useState([]);
+  const [showResultText, setResultText] = useState(false);
+
+  const url = 'https://api.spotify.com/v1/search';
+  const limit = 10;
+  const searchTypes = [
+    'Album',
+    'Artist',
+    'Playlist',
+    'Track',
+    'Show',
+    'Episode',
+  ];
+
+  // When search button is pressed
+  const searchFromSpotify = () => {
+    setResultText(true);
+    axios
+      .get(
+        `${url}?q=${search}&type=${searchType.toLowerCase()}&limit=${limit}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      )
+      .then((response) => {
+        console.log('Got the artist data', response.data);
+        setSearchResult(response.data);
+      })
+      .catch((error) => {
+        console.log('Error', error);
+      });
+  };
+
+  // When content of form is changed
+  const handleSearchChange = (event) => setSearch(event.target.value);
+
+  // Item selected from dropdown
+  const dropDownSelect = (e) => setSearchType(e.toLowerCase());
+
+  const searchButtonClicked = (e) => {
+    e.preventDefault();
+    search.length > 0 ? searchFromSpotify() : console.log('No search query');
+  };
+
+  return (
+    <div>
+      <h2>Search from Spotify</h2>
+      <div>
+        <form>
+          <div>
+            <DropdownButton
+              alignRight
+              title="Change search type"
+              id="dropdown-menu-align-right"
+              variant="secondary"
+              onSelect={dropDownSelect}
+            >
+              {searchTypes.map((type) => (
+                <Dropdown.Item key={type} eventKey={type}>
+                  {type}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+            <p>{`Selected search type is "${searchType}"`}</p>
+            <input value={search} onChange={handleSearchChange} />
+            <button onClick={searchButtonClicked}>Search</button>
+          </div>
+        </form>
+      </div>
+      <p>{showResultText ? 'Top 10 results:' : ''}</p>
+      <ul>
+        <SearchResults results={searchResult} searchType={searchType} />
+      </ul>
+    </div>
+  );
+};
+
+export default SearchFromSpotify;
--- src/index.js
+++ src/index.js
@@ -1,58 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-import SearchForArtist from './component/SearchForArtist'
-import axios from 'axios'
-import './index.css'
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import SearchFromSpotify from './component/SearchFromSpotify';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const App = () => {
-
-  const [accessToken, setToken] = useState('')
-	const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID
-	const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+  const [accessToken, setToken] = useState('');
+  const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 
   // Spotify token
-	useEffect(() => {
-			axios({
-				method: 'post',
-				url:'https://accounts.spotify.com/api/token',
-				data: "grant_type=client_credentials",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-				auth: {
-					username: clientId, // User ID
-					password: clientSecret,  // User Secret
-				},
-			})
-			.then(response => {
-				console.log('Got the token')
-				setToken(response.data.access_token)
-			})
-			.catch(error => {
-				console.log('Error', error);
-			})
-  }, [])
+  useEffect(() => {
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: 'grant_type=client_credentials',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      auth: {
+        username: clientId, // User ID
+        password: clientSecret, // User Secret
+      },
+    })
+      .then((response) => {
+        console.log('Got the token');
+        setToken(response.data.access_token);
+      })
+      .catch((error) => {
+        console.log('Error', error);
+      });
+  }, []);
 
-  return(
-		<>
-    {accessToken === '' ? 'Waiting for token from Spotify' : ''}
-    {accessToken !== '' &&
-      <div>
-        <h1>Spotify Crawler</h1>
-        <p>
-          This is a work in progress project that aims to have functionality to help users 
-          find new music to listen to based on what they have listened to in the past.
-        </p>
-        <h2>Current functionality</h2>
-        <ul>
-          <li>Search for artists</li>
-        </ul>
-        <SearchForArtist accessToken={accessToken}/>
-      </div>
-		}
+  return (
+    <>
+      {accessToken === '' ? 'Waiting for token from Spotify' : ''}
+      {accessToken !== '' && (
+        <div>
+          <h1>Spotify Crawler</h1>
+          <p>
+            This is a work in progress project that aims to have functionality
+            to help users find new music to listen to based on what they have
+            listened to in the past.
+          </p>
+          <h2>Current functionality</h2>
+          <ul>
+            <li>Search from Spotify</li>
+          </ul>
+          <SearchFromSpotify accessToken={accessToken} />
+        </div>
+      )}
     </>
-	)
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
